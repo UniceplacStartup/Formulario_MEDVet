7007fd7d4574399a72fe70b664bd2607b8f2268f
IsaacAiresTh <playerredbrasil@gmail.com>
2025-10-07 19:47:46 -0300

    teste: removendo comentários

--- backend/src/controllers/clinicController.js
+++ backend/src/controllers/clinicController.js
@@ -1,15 +1,11 @@
 // Lógica para o cadastro da clínica (RF01)
 const createClinic = async (req, res) => {
-  // A validação dos dados (CNPJ, email, etc.) deve ser feita aqui
+  
+
+
   const { cnpj, razaoSocial, email, telefone, senha } = req.body;
 
   console.log('Dados recebidos:', req.body);
-
-  // TODO:
-  // 1. Validar os dados de entrada.
-  // 2. Hashear a senha antes de salvar.
-  // 3. Chamar o model para inserir os dados no banco.
-  // 4. Retornar a resposta apropriada.
 
   res.status(201).json({ message: 'Clínica em processo de cadastro!' });
 };
