78b4d48d1ce725752b05bce86c02493e1d137e37
IsaacAiresTh <playerredbrasil@gmail.com>
2025-10-09 13:27:41 -0300

    feat: atualizando Vue components e adicionando declaração de modulos para arquivos .vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,11 @@
-<script>
+// frontend/src/views/HomeView.vue
 
+<script setup>
+import MedVetLogin from '@/components/MedVetLogin.vue'
 </script>
 
 <template>
- 
+  <main>
+    <MedVetLogin />
+  </main>
 </template>
