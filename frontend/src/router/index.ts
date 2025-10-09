1358f33bba6182ca8444f08bc8fb2a7f19e8e2d4
IsaacAiresTh <playerredbrasil@gmail.com>
2025-10-09 14:48:06 -0300

    feature: refactoring Views and components

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,23 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
+import ForgotPasswordView from '../views/ForgotPasswordView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/login'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/login',
+      name: 'login',
+      component: LoginView
     },
-  ],
+    {
+      path: '/cadastro',
+      name: 'cadastro',
+      component: RegisterView
+    },
+    {
+      path: '/esqueci-senha',
+      name: 'esqueci-senha',
+      component: ForgotPasswordView
+    }
+  ]
 })
 
 export default router
