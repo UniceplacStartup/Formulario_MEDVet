c0c43762c3b1645f62151813d864fbf31880067a
ed <eduardodsxavier@gmail.com>
2025-10-12 14:46:55 -0300

    feat: write tests for jwt

---
+++ backend/tests/jwtService.test.js
@@ -0,0 +1,15 @@
+const Clinic = require('../src/models/Clinic');
+const {expect, test} = require('@jest/globals');
+
+const { createToken, getSubjectFromToken} = require('../src/services/jwtService');
+
+test('create token', () => {
+    const clinic = new Clinic.Clinic('1', '2', '3', '4', '5');
+    expect(createToken(clinic)).toContain('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.');
+});
+test('get subject from token', () => {
+    const clinic = new Clinic.Clinic('1', '2', '3', '4', '5');
+    const token = createToken(clinic);
+    const subject = getSubjectFromToken(token); 
+    expect(subject.cnpj).toBe('1');
+});
