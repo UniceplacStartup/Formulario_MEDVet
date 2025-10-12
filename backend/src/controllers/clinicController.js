f73bb94db3c7eb042b5ada049644aa19ac65ef5f
ed <eduardodsxavier@gmail.com>
2025-10-12 14:47:18 -0300

    feat: write jwt service

--- backend/src/controllers/clinicController.js
+++ backend/src/controllers/clinicController.js
@@ -1,8 +1,6 @@
 // Lógica para o cadastro da clínica (RF01)
 const createClinic = async (req, res) => {
   
-
-
   const { cnpj, razaoSocial, email, telefone, senha } = req.body;
 
   console.log('Dados recebidos:', req.body);
